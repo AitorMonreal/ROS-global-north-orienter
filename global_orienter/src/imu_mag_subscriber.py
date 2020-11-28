ea3a3ad4595331f98055a8a36a24ab3ea083adb3
Aitor <monreaitorcelay@gmail.com>
2020-11-28 00:00:39 +0000

    Add print on main & correct issues

--- global_orienter/src/imu_mag_subscriber.py
+++ global_orienter/src/imu_mag_subscriber.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import MagneticField
 
 class MagSubscriber(object):
     def __init__(self):
-        self._mag_sub = rospy.Subscriber("/imu/mag", MagneticField, self.)
+        self._mag_sub = rospy.Subscriber("/imu/mag", MagneticField, self.mag_cb)
         self._mag_data = MagneticField()
 
     def mag_cb(self, msg):
@@ -13,12 +13,16 @@ class MagSubscriber(object):
     
     def get_mag_data(self):
         return self._mag_data
-    
+
+
+# To debug - If this executable script is ran independently through 'rosrun' we will publish the magnetometer data 10 times per second, all x, y and z data. 
+# By aligning the IMU in the North direction, we can see that the x-value on the magnetic field reading approaches 0. Hence this is what we are looking for.
 if __name__ == "__main__":
     rospy.init_node("mag_listener", anonymous=False)
     magsubscriber = MagSubscriber()
     while not rospy.is_shutdown():
         try:
-            magsubscriber.get_mag_data()
+            print(magsubscriber.get_mag_data())
+            rospy.Rate(10).sleep()
         except rospy.ROSInterruptException:
             pass
